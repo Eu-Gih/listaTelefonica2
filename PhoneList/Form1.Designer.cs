b5fe51f7fc17097e288d43fd9d576f01c6b46d36
rbarb <rbarb@FELICIO>
2025-04-03 17:39:25 -0300

    Nova VersãoLista

---
+++ PhoneList/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace PhoneList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btRemove = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(521, 86);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(108, 39);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Nome";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(680, 100);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(680, 182);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPhone.Mask = "(00) 00000-0000";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(132, 22);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(480, 337);
+            this.btAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(168, 57);
+            this.btAdd.TabIndex = 3;
+            this.btAdd.Text = "Adicionar";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // dgvList
+            // 
+            this.dgvList.AllowUserToAddRows = false;
+            this.dgvList.AllowUserToDeleteRows = false;
+            this.dgvList.BackgroundColor = System.Drawing.Color.Violet;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Nome,
+            this.Telefone});
+            this.dgvList.Location = new System.Drawing.Point(16, 15);
+            this.dgvList.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.ReadOnly = true;
+            this.dgvList.RowHeadersWidth = 51;
+            this.dgvList.Size = new System.Drawing.Size(395, 524);
+            this.dgvList.TabIndex = 4;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(488, 169);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(150, 39);
+            this.lbl2.TabIndex = 5;
+            this.lbl2.Text = "Telefone";
+            // 
+            // btRemove
+            // 
+            this.btRemove.Location = new System.Drawing.Point(745, 337);
+            this.btRemove.Margin = new System.Windows.Forms.Padding(4);
+            this.btRemove.Name = "btRemove";
+            this.btRemove.Size = new System.Drawing.Size(168, 57);
+            this.btRemove.TabIndex = 7;
+            this.btRemove.Text = "Remover";
+            this.btRemove.UseVisualStyleBackColor = true;
+            this.btRemove.Click += new System.EventHandler(this.btRemove_Click);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 125;
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.MinimumWidth = 6;
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
+            this.Nome.Width = 125;
+            // 
+            // Telefone
+            // 
+            this.Telefone.HeaderText = "Telefone";
+            this.Telefone.MinimumWidth = 6;
+            this.Telefone.Name = "Telefone";
+            this.Telefone.ReadOnly = true;
+            this.Telefone.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btRemove);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.dgvList);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.DataGridView dgvList;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btRemove;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+    }
+}
+
---
+++ PhoneList/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneList
+{
+    public partial class Form1 : Form
+    {
+        string[][] lista;
+        readonly int max = 100;
+        string identificador = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            lista = new string[max][];
+        }
+
+        int Tamanho(string[][] matriz)
+        {
+            int itens = 0;
+            for (int i = 0; i < matriz.Length; i++)
+            {
+                if (matriz[i] != null)
+                {
+                    itens++;
+                }
+            }
+            return itens;
+        }
+
+        int Tamanho(string[] vetor)
+        {
+            int itens = 0;
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] != null)
+                {
+                    itens++;
+                }
+            }
+            return itens;
+        }
+
+        void Atualizar()
+        {
+            dgvList.Rows.Clear();
+            for (int i = 0; i < Tamanho(lista); i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dgvList);
+                for (int j = 0; j < Tamanho(lista[i]); j++)
+                {
+                    row.Cells[j].Value = lista[i][j];
+                }
+                dgvList.Rows.Add(row);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) || !txtPhone.MaskFull)
+            {
+                MessageBox.Show("Insira nome e telefone!");
+                return;
+            }
+
+            if (identificador != "")
+            {
+                for (int i = 0; i < Tamanho(lista); i++)
+                {
+                    if (lista[i][0] == identificador)
+                    {
+                        lista[i][1] = txtName.Text;
+                        lista[i][2] = txtPhone.Text;
+                        break;
+                    }
+                }
+                identificador = "";
+            }
+            else
+            {
+                int novoId = Tamanho(lista) + 1;
+                lista[Tamanho(lista)] = new string[] { novoId.ToString(), txtName.Text, txtPhone.Text };
+            }
+
+            txtName.Clear();
+            txtPhone.Clear();
+            Atualizar();
+        }
+
+        private void btRemove_Click(object sender, EventArgs e)
+        {
+            DataGridViewCell celula = dgvList.SelectedCells[0];
+            int linha = celula.RowIndex;
+            string idRemover = dgvList.Rows[linha].Cells[0].Value.ToString();
+
+            int indice = 0;
+            for (indice = 0; indice < Tamanho(lista) && lista[indice][0] != idRemover; indice++) ;
+
+            DialogResult resultado = MessageBox.Show("Deseja realmente remover?", "Remover", MessageBoxButtons.YesNo);
+            if (resultado == DialogResult.Yes)
+            {
+                for (int i = indice; i < Tamanho(lista) - 1; i++)
+                {
+                    lista[i] = lista[i + 1];
+                }
+                lista[Tamanho(lista) - 1] = null;
+
+                for (int i = 0; i < Tamanho(lista); i++)
+                {
+                    lista[i][0] = (i + 1).ToString(); // Atualiza os IDs
+                }
+
+                Atualizar();
+            }
+        }
+
+        private void dgvList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dgvList.Rows.Count)
+            {
+                DataGridViewRow linha = dgvList.Rows[e.RowIndex];
+                identificador = linha.Cells[0].Value.ToString();
+                txtName.Text = linha.Cells[1].Value.ToString();
+                txtPhone.Text = linha.Cells[2].Value.ToString();
+            }
+        }
+    }
+}
